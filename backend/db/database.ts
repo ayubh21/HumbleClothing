20b5acdfc6f1604c09055b4e6e945a21ea6e1a2e
ayubh21 <112664785+ayubh21@users.noreply.github.com>
2024-03-13 04:25:13 -0400

    added global postgres connection

--- backend/db/database.ts
+++ backend/db/database.ts
@@ -1,1 +1,11 @@
 // global client
+// create db func to handle connections to database
+
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString:
+    "postgresql://postgres:tusay101@localhost:5432/postgres?schema=public",
+});
+
+const db = function queryClient(db: Client): Client {};
